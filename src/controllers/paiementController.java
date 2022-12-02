1bf052c9ccb7ff75f9669b1f88b3aa0220155cde
neimael <114741266+neimael@users.noreply.github.com>
2022-12-02 16:46:26 +0100

    add show card_code or hide && generate ticket for after payment

--- src/controllers/paiementController.java
+++ src/controllers/paiementController.java
@@ -4,9 +4,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -15,10 +20,16 @@ import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
 import ConnectionDB.ControlDB;
 import javafx.event.ActionEvent;
 
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.input.MouseEvent;
@@ -30,10 +41,35 @@ public class paiementController {
 	private Parent fxml;
 
 	@FXML
-	private TextField card_number;
+    private ToggleButton show_btn;
+	
 	@FXML
-	private TextField card_code;
+    private TextField pass_text;
+	
+	@FXML
+	private TextField card_number;
+	
+	@FXML
+	private PasswordField card_code;
 
+	
+	@FXML
+    private ImageView show_pass;
+	
+	@FXML
+    void show(ActionEvent event) {
+    	if(show_btn.isSelected()) {
+    		show_pass.setImage(new Image ("img/invisile.png"));
+    		pass_text.setText(card_code.getText());
+    		pass_text.setVisible(true);
+    		return;
+    	}
+    	else {
+    		show_pass.setImage(new Image ("img/visible.png"));
+    		card_code.setText(pass_text.getText());
+    		pass_text.setVisible(false);
+    	}
+    }
 	
 	private Connection conn;
     private PreparedStatement statement;
@@ -41,7 +77,7 @@ public class paiementController {
     
 	// Event Listener on Button.onAction
 	@FXML
-	public void insert_pin(ActionEvent event) throws SQLException, IOException {
+	public void insert_pin(ActionEvent event) throws SQLException, IOException, DocumentException {
     	
     	conn = ControlDB.createConnection();
     	
@@ -54,9 +90,23 @@ public class paiementController {
 		 
 		 if(result.next()) {
 			 
+			 Document doc = new Document();
+			 PdfWriter writer = PdfWriter.getInstance(doc, new FileOutputStream("src/pdf_ticket/ticket.pdf"));
+			 doc.open();
+			 Paragraph para = new Paragraph("Ticket of Travel !");
+			 doc.add(para);
+			 doc.close();
+			 writer.close();
+			 
+			 System.out.println("Ticket generated Successfully !");
+			 
+		     Stage choice = new Stage();
+
 			 
 			 JOptionPane.showMessageDialog(null, "Successfully paid !", "Success Message",JOptionPane.INFORMATION_MESSAGE);
 			
+
+
 		    	
 		 }
 		 else {
