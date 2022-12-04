4909ca6a99d2341a79079ac9b498d6e537a1e3d3
neimael <114741266+neimael@users.noreply.github.com>
2022-12-04 20:48:46 +0100

    search travel && combobox && spinner with database

--- src/controllers/SearchTravelController.java
+++ src/controllers/SearchTravelController.java
@@ -1,55 +1,96 @@
 package controllers;
 
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
 
+import javax.swing.JOptionPane;
+
+import Model.Travel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DateCell;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-public class SearchTravelController {
+public class SearchTravelController implements Initializable{
 
-    @FXML
-    private ChoiceBox<?> arr_city;
+	@FXML
+    private ComboBox<String> arr_city;
 
     @FXML
     private DatePicker date_travel;
 
     @FXML
-    private ChoiceBox<?> dep_city;
+    private ComboBox<String> dep_city;
 
     @FXML
-    private Spinner<?> num_ticket;
+    private Spinner<Integer> num_ticket;
 
     @FXML
     private AnchorPane searchT;
 
 	private Parent fxml;
+	
+	
 
     @FXML
     void search_travel(ActionEvent event) {
-    	searchT.getScene().getWindow().hide();
-    	Stage choice = new Stage();
-    	try {
-    		fxml = FXMLLoader.load(getClass().getResource("/application/travel.fxml"));
-    		Scene scene = new Scene(fxml);
-    		choice.initStyle(StageStyle.UNDECORATED);
-    		choice.setScene(scene);
-    		Image image = new Image("img/icon.png");
-			choice.getIcons().add(image);
-    		choice.setResizable(false);
-    		choice.show();
-    	} catch(IOException e) {
-    		e.printStackTrace();
-    	}
+    	
+    	
+    	
+    	
+    		String selected_dep = dep_city.getSelectionModel().getSelectedItem().toString();
+    		Travel.setCity_dep(selected_dep);
+    		
+    		String selected_arr = arr_city.getSelectionModel().getSelectedItem().toString();
+    		Travel.setCiy_arr(selected_arr);
+    		
+    		int current_value = num_ticket.getValue();
+    		Travel.setNum_places(current_value);
+    		
+    		
+    		LocalDate myDate = date_travel.getValue();
+    		Travel.setDate_travel(myDate);
+    		
+    		
+    		if(selected_arr.equals(selected_dep)) {
+				 JOptionPane.showMessageDialog(null, "Departure City Shouldn't be the same As Arrival City !", "Error Message",JOptionPane.ERROR_MESSAGE);
+    		}
+    		else {
+    			try {
+    				Stage choice = new Stage();
+    			fxml = FXMLLoader.load(getClass().getResource("/application/travel.fxml"));
+        		Scene scene = new Scene(fxml);
+        		choice.initStyle(StageStyle.UNDECORATED);
+        		choice.setScene(scene);
+        		Image image = new Image("img/icon.png");
+    			choice.getIcons().add(image);
+        		choice.setResizable(false);
+        		choice.show();
+        		
+    		} catch(IOException e) {
+        		e.printStackTrace();
+        	} 
+    			searchT.getScene().getWindow().hide();
+    	    	
+    			}
+    		
+    		
+    		
+    		
+    	
     }
     
     @FXML
@@ -69,5 +110,26 @@ public class SearchTravelController {
     		e.printStackTrace();
     	}
     }
+
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		String [] cities = {"Agadir","Casablanca","Rabat","Tanger","Marrakech","El Jadida","Asfi","Fès"};
+		arr_city.getItems().addAll(cities);
+		dep_city.getItems().addAll(cities);
+		
+		SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 10);
+		valueFactory.setValue(1);
+		num_ticket.setValueFactory(valueFactory);
+		
+		date_travel.setDayCellFactory(param -> new DateCell() {
+		    private LocalDate now = LocalDate.now();
+
+		    @Override
+		    public void updateItem(LocalDate date, boolean empty) {
+		        super.updateItem(date, empty);
+		         setDisable(date.compareTo(now) < 0 );
+		    }
+		});
+		
+	}
 
 }
--- src/controllers/travelController.java
+++ src/controllers/travelController.java
@@ -1,40 +1,58 @@
 package controllers;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Time;
+import java.util.ResourceBundle;
 
+
+import ConnectionDB.ControlDB;
+import Model.Travel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-public class travelController {
+public class travelController implements Initializable {
 
     @FXML
-    private TableColumn<?, ?> arr_time;
+    private TableColumn<Travel, Time> arr_time;
 
     @FXML
-    private TableColumn<?, ?> dep_time;
+    private TableColumn<Travel, Time> dep_time;
+    
+    private Connection conn;
+    private PreparedStatement statement;
+    private ResultSet result = null;
 
     @FXML
-    private TableColumn<?, ?> name_company;
+    private TableColumn<Travel, String> name_company;
 
     @FXML
-    private TableColumn<?, ?> price;
+    private TableColumn<Travel, Float> price;
 
     @FXML
     private AnchorPane result_travel;
 
-    @FXML
-    private TableColumn<?, ?> route;
-
 	private Parent fxml;
+	
+	 @FXML
+	 private TableView<Travel> search_travel;
 
     @FXML
     void reserver(ActionEvent event) {
@@ -71,5 +89,50 @@ public class travelController {
     		e.printStackTrace();
     	}
     }
+    
+    
+    private void load(){
+        ObservableList<Travel> data = FXCollections.observableArrayList();
 
+    	conn = ControlDB.createConnection();
+
+        data = FXCollections.observableArrayList();
+        try{
+        	
+        	String sql = "SELECT company_name,depart_time,arriving_time,price FROM travel WHERE villedepart = ? and villedarriver = ? and date_travel = ? and number_places >= ? ";
+    		
+   		 	statement = conn.prepareStatement(sql);
+   		 	statement.setString(1, Travel.getCity_dep());
+   		 	statement.setString(2, Travel.getCiy_arr());
+   		 	statement.setObject(3, Travel.getDate_travel());
+   		 	statement.setInt(4, Travel.getNum_places());
+   		 	result = statement.executeQuery();
+		      
+         while(result.next()){
+            data.add(new Travel(result.getString("company_name"),result.getTime("depart_time"),result.getTime("arriving_time"),Float.parseFloat(result.getString("price")) ));
+            search_travel.setItems(data);
+         }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+    public void showListe() {
+
+    	name_company.setCellValueFactory(new PropertyValueFactory<Travel,String>("company_name"));
+    	dep_time.setCellValueFactory(new PropertyValueFactory<Travel,Time>("dep_time"));
+    	arr_time.setCellValueFactory(new PropertyValueFactory<Travel,Time>("arr_time"));
+    	price.setCellValueFactory(new PropertyValueFactory<Travel,Float>("price"));
+    }
+    
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		showListe();
+		load();
+	}
+
+	
 }
